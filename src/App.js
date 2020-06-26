3940cecb5b5080c5ca4c82533b7200ea45ed6af8
NishithaJ09 <nishitha.jarubula09@gmail.com>
2020-06-26 16:16:47 -0500

    weather app using react hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const api = {
+  key: "475dcff19c6e96a3c3e6bce0f43036cb",
+  base: "https://home.openweathermap.org/api_keys"
+}
+const dateBuilder = (d) => {
+let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+let dataObj = new Date();
+let hours = dataObj.getHours();
+let minutes = dataObj.getMinutes();
+let seconds = dataObj.getSeconds();
+let date = dataObj.getUTCDate();
+let month = months[dataObj.getUTCMonth() + 1];
+let year = dataObj.getUTCFullYear();
+var day = days[
+  dataObj.getDay() ];
+let amOrPm = hours> 12? 'PM' : 'AM';
+  return `${day} ${date} ${month} ${year} at ${hours} : ${minutes} : ${seconds} ${amOrPm}`
+}
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text"
+          className="search-bar"
+          placeholder="search.."
+        />
+    </div>
+    <div className="location-box">
+      <div className="location">New York City,US </div>
+      <div className="date">{dateBuilder(new Date())} </div>
+      </div>
+    <div className="weather-box">
+      <div className="temp">
+        15C
+      </div>
+      <div className="weather">
+        sunny
+      </div>
+
+      </div>
+
+      </main>
     </div>
   );
 }
